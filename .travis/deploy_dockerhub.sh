ab1b8cf5bcc5c594b4eb5ce28df2f9ff4bed20df
MikeOsa123 <m.osamwonyi@gmail.com>
2020-10-16 18:13:20 +0100

    modified deploy_dockerhub.sh

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -3,7 +3,7 @@ docker login -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
-    TAG="$TRAVIS_BRANCH"
+    TAG="feature"
 fi
 REPO="/cicd-buzz"
 docker build -f Dockerfile -t $DOCKER_USER$REPO:$TAG .
